90cc884cdbe87e381b371dfa4ad856aaab48fe5b
Deepak <aryanrajj1234@gmail.com>
2024-02-05 13:57:06 +0530

    complete-socket-io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,125 @@
-import React from 'react'
-import { useEffect } from 'react'
-import {io} from 'socket.io-client'
+import React, { useMemo, useState } from "react";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const App = () => {
-  const socket = io('http://localhost:2000')
-  useEffect(()=>{
-    socket.on("connect",()=>{
-      console.log("connected",socket.id)
-    })
-  },[])
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+  // continuous backend re-render nhi hoga when change input keyword----only change when  we refresh page
+  const socket = useMemo(
+    () => io("http://localhost:2000", { withCredentials: true }),
+    []
+  );
 
-export default App
+  // when change input keybord again again re-render backend
+  // const socket = io("http://localhost:2000");
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketID] = useState("");
+  const [receivemessage, setRecieveMessage] = useState([]);
+  const [roomName, setRoomName] = useState("");
+
+  console.log(receivemessage);
+
+  console.log(message);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+
+    setMessage("");
+  };
+
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
+  };
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setSocketID(socket.id);
+      console.log("connected", socket.id);
+    });
+    /* socket on listner */
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+
+    socket.on("welcome", (s) => {
+      console.log(s);
+    });
+    socket.on("receive-message", (data) => {
+      console.log("receive-message", data);
+      setRecieveMessage((receivemessage) => [...receivemessage, data]);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ height: 500 }} />
+      <Typography variant="h4" component="div" gutterBottom>
+        Welcome to socket.io
+      </Typography>
+
+      <Typography variant="h4" component="div" gutterBottom>
+        {socketID}
+      </Typography>
+
+      {/* form when user join room */}
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+      </form>
+
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Romm-Message"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+      <Stack>
+        {receivemessage.map((m, index) => (
+          <Typography key={index} variant="h6" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
+    </Container>
+  );
+};
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser";
+
 const port = 2000;
+const secretKey = "deep"
 
 const app = express();
 const server = createServer(app);
@@ -21,13 +25,65 @@ const io = new Server(server, {
 server.listen(port, () => {
   console.log(`Server is Running on Port ${port}`);
 });
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello WOrld ");
 });
 
+app.get("/login", (req, res) => {
+  const token = jwt.sign({_id:"dhsdfsdfhsflafa"},secretKey);
+
+  res.cookie("token",token,{httpOnly:true,secure:true,sameSite:"none"})
+  .json({
+    message:"Login Success",
+  })
+});
+// this is middleware socket io---->authenticate ke liye use kar skte hai
+const user = false;
+io.use((socket,next)=>{
+  cookieParser()(
+    socket.request,socket.request.res,(err)=>{
+      if(err)
+      return next(err);
+    const token  = socket.request.cookies.token;
+    if(!token)
+      return next(new Error("Authentication Error"))
+      const decoded = jwt.verify(token,secretKey);
+      next();
+});
+
+})
+
 // yha par ak circuit bna liya
 // socket -->> individual socket
 io.on("connection", (socket) => {
-  console.log("User Connected");
-  console.log("ID", socket.id);
+  console.log("User Connected", socket.id);
+
+  /* socket.emit me ak particular socket ko message jayega */
+  // socket.emit("welcome",`Welcome to the server ${socket.id}`);
+
+  /* socket broadcast means us socket ko message nhi jayega jo web reload karega aur sbhi ko jayega  isase pta chal jayega ki kon server login kiya hai */
+  // socket.broadcast.emit("welcome",`join the server ${socket.id}`);
+
+  // listner
+  socket.on("message", ({message,room}) => {
+    console.log({message,room});
+    // io.emit("receive-message",data);
+
+    // socket.broadcast.emit("receive-message",data);
+    /* IO.to bhi use kar skte hai  */
+  socket.to(room).emit("receive-message",message);
+    
+  });
+
+  socket.on("join-room",(room)=>{
+    socket.join(room);
+    console.log(`User Joined Room ${room}`)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Disconnect the ", socket.id);
+  });
 });
